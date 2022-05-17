7e9fd4a400ae62121070dba5de868783ccc48190
joyannavb <joyannavb@gmail.com>
2022-05-16 20:03:24 -0500

    initial

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionStatements
+{
+    internal class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            int favorite = 22;
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < favorite)
+            {
+                Console.WriteLine("Too low.");
+            }
+            else if (userGuess > favorite)
+            {
+                Console.WriteLine("Too high.");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+
+        }
+    }
+}
